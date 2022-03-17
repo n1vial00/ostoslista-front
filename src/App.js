c7124e968138537c55d18d7df6cd1f97c8243a34
Aleksi Viitanen <n1vial00@students.oamk.fi>
2022-03-17 14:35:57 +0200

    Working on structure

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ const URL = 'http://localhost/Ostoslista-PHP/';
 
 function App() {
 
+  function addData(e) {
+    e.preventDefault();
+    
+
+  }
+
   const [data, setData] = useState("");
   useEffect(() => {
     axios.get(URL)
@@ -19,9 +25,22 @@ function App() {
   });
 
   return (
-    <div> 
+    <div className='app'> 
     <h1>Here's some data hopefully:</h1> 
     <p>{data}</p>
+    
+
+    <form onSubmit={addData}>
+      
+      <div>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+      </div>
+      
+      <div><button>kek</button></div>
+    </form>
+
     </div>
     
   );
