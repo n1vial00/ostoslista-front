342fa98d8be7cc1d53207ca696fcacf9baf428d2
Aleksi Viitanen <n1vial00@students.oamk.fi>
2022-03-17 14:08:37 +0200

    Extracting test data success

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const URL = 'http://localhost/Ostoslista-PHP/';
 
 
 
 function App() {
+
+  const [data, setData] = useState("");
+  useEffect(() => {
+    axios.get(URL)
+      .then((response) => {
+        setData(response.data[0].description);
+      }).catch(error => {
+        alert(error);
+      });
+  });
+
   return (
-    <div className="app"> test div </div>
+    <div> 
+    <h1>Here's some data hopefully:</h1> 
+    <p>{data}</p>
+    </div>
+    
   );
 }
 
